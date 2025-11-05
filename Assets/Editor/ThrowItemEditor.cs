99a7f54426f4534b36c0e31864a8af1d44310c52
m24x7 <59781877+m24x7@users.noreply.github.com>
2025-11-05 14:36:04 -0700

    Added Throw Item class and editor, no implementation

---
+++ Assets/Editor/ThrowItemEditor.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace GDD3400.Labyrinth
+{
+    /// <summary>
+    /// This class provides a custom editor for the Throw Item component to visualize it in the scene view
+    /// </summary>
+    [CustomEditor(typeof(ThrowItem))]
+    public class ThrowItemEditor : Editor
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/GDD 3400 - The Labyrinth/Scripts/Player/ThrowItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace GDD3400.Labyrinth
+{
+    /// <summary>
+    /// This class handles the logic for throwing items/objects.
+    /// </summary>
+    public class ThrowItem : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
